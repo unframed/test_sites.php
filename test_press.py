33825ac7684280319d74078fffeabbf00a8a908b
Laurent Szyster <laurentszyster@gmail.com>
2014-10-20 12:21:47 +0200

    fix issue #20, add support for a dbHost option in test_sites.json

--- test_press.py
+++ test_press.py
@@ -7,13 +7,13 @@ class TestPress (test_sites.TestSite):
 
     def mysqlImport (self):
         return test_sites.mysql_import(
-            self.path, self.getMySQLUser(), 'dummy', self.name, 
+            self.path, self.getMySQLUser(), 'dummy', self.name,
             ' | sed s,TEST_SITES_HOST,{0},g | '.format(self.getHttpHost())
             )
 
     def mysqlDump (self):
         return test_sites.mysql_dump(
-            self.path, self.getMySQLUser(), 'dummy', self.name, 
+            self.path, self.getMySQLUser(), 'dummy', self.name,
             ' | sed s,{0},TEST_SITES_HOST,g | '.format(self.getHttpHost())
             )
 
@@ -23,10 +23,16 @@ class TestPress (test_sites.TestSite):
         wp_config = self.path+'/run/wp-config.php'
         if test_sites.file_exists(wp_config):
             configuration = re.sub(
-                r"define[(]'DB_NAME', '.+?'[)];", 
+                r"define[(]'DB_NAME', '.+?'[)];",
                 "define('DB_NAME', '{0}');".format(self.name),
                 open(wp_config).read()
                 )
+            if self.options.has_key('dbHost'):
+                configuration = re.sub(
+                    r"define[(]'DB_HOST', '.+?'[)];",
+                    "define('DB_HOST', '{0}');".format(self.options['dbHost']),
+                    configuration
+                    )
             open(wp_config, 'w').write(configuration)
 
 if __name__ == '__main__':
