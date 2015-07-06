f99a7200be2c9e3c17746a2dccf57bcaba4ea2ae
Laurent Szyster <laurentszyster@gmail.com>
2015-07-06 16:36:19 +0200

    #24 install the php5-mysql package

--- test/units/wp_install.js
+++ test/units/wp_install.js
@@ -2,7 +2,7 @@
  * Setup and install WordPress.
  */
 
-casper.test.begin('Install WordPress', 2, function suite (test) {
+casper.test.begin('Install WordPress', 17, function suite (test) {
     var test_sites_name = casper.cli.options["name"],
         test_sites_host = casper.cli.options["host"],
         test_sites_mysqluser = casper.cli.options["mysqluser"],
@@ -10,7 +10,6 @@ casper.test.begin('Install WordPress', 2, function suite (test) {
     // test_sites_out = 'test/sites/'+test_sites_name+'/out/'
     casper.start();
     casper.wait(1000);
-    /*
     casper.thenOpen(test_sites_root+'/wp-admin/setup-config.php?step=1', function () {
         // this.capture(test_sites_out+'wp_config_form.png');
         test.assertHttpStatus(200, 'GET '+this.getCurrentUrl());
@@ -30,7 +29,11 @@ casper.test.begin('Install WordPress', 2, function suite (test) {
             'input[name="prefix"]': 'wp_'
         }, true);
     });
-    */
+    casper.then(function () {
+        // this.capture(test_sites_out+'wp_config_response.png');
+        test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
+    });
+    /*
     casper.thenOpen(test_sites_root+'/wp-admin/setup-config.php?step=2', {
         'method': 'post',
         'data': {
@@ -44,11 +47,7 @@ casper.test.begin('Install WordPress', 2, function suite (test) {
         test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
         this.debugPage();
     });
-    /*
-    casper.then(function () {
-        // this.capture(test_sites_out+'wp_config_response.png');
-        test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
-    });
+    */
     casper.thenOpen(test_sites_root+'/wp-admin/install.php', function () {
         // this.capture(test_sites_out+'wp_install_form.png');
         test.assertHttpStatus(200, 'GET '+this.getCurrentUrl());
@@ -74,7 +73,7 @@ casper.test.begin('Install WordPress', 2, function suite (test) {
         // this.capture(test_sites_out+'wp_install_response.png');
         test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
     });
-    */
+    /*
     casper.thenOpen(test_sites_root+'/wp-admin/install.php?step=2', {
         'method': 'post',
         'data': {
@@ -89,6 +88,7 @@ casper.test.begin('Install WordPress', 2, function suite (test) {
         test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
         this.debugPage();
     });
+    */
     casper.run(function () {
         this.test.done();
     });
