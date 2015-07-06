71bd574bbfa16789d5e0c8a3599afa257df5a3e1
Laurent Szyster <laurentszyster@gmail.com>
2015-07-06 16:16:26 +0200

    #24 directly POST, comment out forms

--- test/units/wp_install.js
+++ test/units/wp_install.js
@@ -2,7 +2,7 @@
  * Setup and install WordPress.
  */
 
-casper.test.begin('Install WordPress', 17, function suite (test) {
+casper.test.begin('Install WordPress', 2, function suite (test) {
     var test_sites_name = casper.cli.options["name"],
         test_sites_host = casper.cli.options["host"],
         test_sites_mysqluser = casper.cli.options["mysqluser"],
@@ -10,17 +10,18 @@ casper.test.begin('Install WordPress', 17, function suite (test) {
     // test_sites_out = 'test/sites/'+test_sites_name+'/out/'
     casper.start();
     casper.wait(1000);
+    /*
     casper.thenOpen(test_sites_root+'/wp-admin/setup-config.php?step=1', function () {
         // this.capture(test_sites_out+'wp_config_form.png');
         test.assertHttpStatus(200, 'GET '+this.getCurrentUrl());
-    });
-    casper.then(function () {
         test.assertExists('form');
         test.assertExists('input[name="dbname"]');
         test.assertExists('input[name="uname"]');
         test.assertExists('input[name="pwd"]');
         test.assertExists('input[name="dbhost"]');
         test.assertExists('input[name="prefix"]');
+    });
+    casper.then(function () {
         this.fillSelectors('form', {
             'input[name="dbname"]': test_sites_name,
             'input[name="uname"]': test_sites_mysqluser,
@@ -29,6 +30,20 @@ casper.test.begin('Install WordPress', 17, function suite (test) {
             'input[name="prefix"]': 'wp_'
         }, true);
     });
+    */
+    casper.thenOpen(test_sites_root+'/wp-admin/setup-config.php?step=2', {
+        'method': 'post',
+        'data': {
+            'dbname': test_sites_name,
+            'uname': test_sites_mysqluser,
+            'pwd': 'dummy',
+            'dbhost': 'localhost',
+            'prefix': 'wp_'
+        }
+    }, function () {
+        test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
+    });
+    /*
     casper.then(function () {
         // this.capture(test_sites_out+'wp_config_response.png');
         test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
@@ -36,8 +51,6 @@ casper.test.begin('Install WordPress', 17, function suite (test) {
     casper.thenOpen(test_sites_root+'/wp-admin/install.php', function () {
         // this.capture(test_sites_out+'wp_install_form.png');
         test.assertHttpStatus(200, 'GET '+this.getCurrentUrl());
-    });
-    casper.then(function () {
         test.assertExists('form#setup');
         test.assertExists('input#weblog_title');
         test.assertExists('input#user_login');
@@ -45,6 +58,8 @@ casper.test.begin('Install WordPress', 17, function suite (test) {
         test.assertExists('input#pass2');
         test.assertExists('input#admin_email');
         test.assertExists('input[name="blog_public"]');
+    });
+    casper.then(function () {
         this.fillSelectors('form#setup', {
             'input#weblog_title': test_sites_name,
             'input#user_login': 'admin',
@@ -56,6 +71,20 @@ casper.test.begin('Install WordPress', 17, function suite (test) {
     });
     casper.then(function () {
         // this.capture(test_sites_out+'wp_install_response.png');
+        test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
+    });
+    */
+    casper.thenOpen(test_sites_root+'/wp-admin/install.php?step=2', {
+        'method': 'post',
+        'data': {
+            'weblog_title': test_sites_name,
+            'user_name': test_sites_mysqluser,
+            'admin_password': 'dummy',
+            'admin_password2': 'dummy',
+            'admin_email': 'staff@mailpoet.com',
+            'blog_public': '0'
+        }
+    }, function () {
         test.assertHttpStatus(200, 'POST '+this.getCurrentUrl());
     });
     casper.run(function () {
